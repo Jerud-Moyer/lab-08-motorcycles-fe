59d64f955f3f602342388586a5cf2987c614adbd
Jerud-Moyer <jerudmoyer@gmail.com>
2020-08-13 21:15:09 -0700

    sleepy time

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -20,7 +20,7 @@ export default class DetailPage extends Component {
     render() {
         return (
             <div className='result-box'>
-                <h1>Check out your rad motorcycle!</h1>
+                <h1 className='rad'>Check out your rad motorcycle!</h1>
                 {`model - ${this.state.motorcycle.model}`} <br/> {`make - ${this.state.motorcycle.manufacturer}`}<br/> {`type - ${this.state.motorcycle.type}`}<br/>  {`this bike is fast! - ${this.state.motorcycle.is_fast}`}<br/> {`engine size - ${this.state.motorcycle.ccs}`}
             </div>
         )
